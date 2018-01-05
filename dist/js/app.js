3da7ea072aae511db60c937e421f825180121173
martinlex <info@lexelius.se>
2018-01-06 00:53:20 +0100

    Add rendered query

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,12 +3,14 @@ var vm = new Vue({
   data: {
     imdbUrl: 'http://www.imdb.com/title/',
     shows: [],
-    query: ''
+    query: '',
+    renderedQuery: ''
   },
   methods: {
     getShows: function(q) {
       axios.get('http://api.tvmaze.com/search/shows?q=' + q)
         .then(response => {this.shows = response.data})
+      this.renderedQuery = this.query;
       this.query = '';
     }
   }
